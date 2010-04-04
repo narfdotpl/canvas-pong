c9321e4a7f248b8a40e82c1fd481e7b5ae1f06f5
Maciej Konieczny <hello@narf.pl>
2010-04-04 20:53:41 +0200

    added keypress detection

--- src/pong.js
+++ src/pong.js
@@ -3,6 +3,18 @@ function main() {
     if (canvas.getContext) {
         var c = canvas.getContext('2d');
 
+        // remember whether key is pressed
+        var keys = {};
+        $(document).keydown(function(e) { keys[e.keyCode] = true; });
+        $(document).keyup(function(e) { keys[e.keyCode] = false; });
+
+        // return function telling if key is pressed
+        function isPressed(keyCode) {
+            return function() {
+                keys[keyCode];
+            };
+        }
+
         // set border line parameters
         var borderThickness = 2;
         c.fillStyle = '#000';
