3cd00bb960cd3aa237bbad6e94db182fb8ccfe25
Maciej Konieczny <hello@narf.pl>
2010-04-04 20:01:54 +0200

    added borders

--- src/pong.js
+++ src/pong.js
@@ -1,1 +1,17 @@
+function main() {
+    var canvas = $('canvas#pong')[0];
+    if (canvas.getContext) {
+        var c = canvas.getContext('2d');
 
+        // set border line parameters
+        var borderThickness = 2;
+        c.fillStyle = '#000';
+
+        // draw top and bottom border lines
+        c.fillRect(0, 0, canvas.width, borderThickness);
+        c.fillRect(0, canvas.height - borderThickness, canvas.width,
+                   borderThickness);
+    }
+}
+
+main();
